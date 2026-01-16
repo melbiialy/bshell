ba3885172d3aafc891aea1a63df713d88be0a5e5
Elbialy0 <mahmoudelbialy109@gmail.com>
2026-01-16 13:32:50 +0200

    log command path lookup result in CommandRegistry for better debugging

--- src/main/java/CommandRegistry.java
+++ src/main/java/CommandRegistry.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +24,8 @@ public class CommandRegistry {
             Process process = Runtime.getRuntime().exec(new String[]{
                     "which", commandName
             });
+            BufferedReader processReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            System.out.println( commandName +" is " + processReader.readLine());
             if (commandRegistry.contains(commandName)) {
                 System.out.println(commandName + " is a shell builtin");
                 return;
