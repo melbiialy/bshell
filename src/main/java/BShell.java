f756db1e253c4b01aa3695fb982fb761b7fc9323
Elbialy0 <mahmoudelbialy109@gmail.com>
2026-01-16 13:25:25 +0200

    refactor BShell to centralize command parsing and execution logic, streamline Main class initialization

--- src/main/java/BShell.java
+++ src/main/java/BShell.java
@@ -1,13 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class BShell {
     public static Path path;
-    CommandRegistry commandRegistry;
+    private CommandRegistry commandRegistry;
+    private CommandParser commandParser;
     public BShell() {
         path = Path.of(System.getProperty("user.dir"));
-        // todo add helper method to register the builtin commands
         commandRegistry = CommandRegistry.registerBuiltinCommands();
+        commandParser = new CommandParserImp(commandRegistry);
     }
     public Path getPath() {
         return path;
@@ -25,7 +29,37 @@ public class BShell {
     public void moveBackward(){
         this.moveTo(path.resolve(".."));
     }
-    public void start(){
+    public void start() throws IOException {
+        System.out.println("Welcome to BShell");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        System.out.print("$ ");
+        while ((input = reader.readLine())!=null){
+            Command command;
+            try {
+                command = commandParser.parse(input);
+            } catch (CommandNotFound e) {
+                Process process = Runtime.getRuntime().exec(new String[]{"which",input.split(" ")[0]});
+                BufferedReader processReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line = processReader.readLine();
+                if (line != null) {
+                    Process execProcess = Runtime.getRuntime().exec(input);
+                    BufferedReader execReader = new BufferedReader(new InputStreamReader(execProcess.getInputStream()));
+                    String execLine;
+                    while ((execLine = execReader.readLine()) != null) {
+                        System.out.println(execLine);
+                    }
+                } else {
+                    System.out.println(input.split(" ")[0] + ": command not found");
+                }
+                System.out.print("$ ");
+                continue;
+            }
+            command.execute(input.split(" "));
+            System.out.print("$ ");
+
+
+        }
 
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,64 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Command commandR = new Command("exit",(a)->System.exit(0));
-        Command echo = new Command("echo",(a)->{
-            System.out.println(String.join(" ",a).substring(4).trim());
-        });
-        CommandRegistry commandRegistry = new CommandRegistry();
-        commandRegistry.register(echo);
-        commandRegistry.register(commandR);
-        Command type = new Command("type",(a)->{
-            if (a.length < 2) {
-                System.out.println("type: missing operand");
-                return;
-            }
-            String commandName = a[1];
-            Process process = Runtime.getRuntime().exec(new String[]{
-                    "which",commandName
-            });
-            if (commandRegistry.contains(commandName)) {
-                System.out.println(commandName + " is a shell builtin");
-                return;
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = reader.readLine();
-            if (line != null){
-                System.out.println(commandName + " is " + line);
-            }
-            else {
-                System.out.println(commandName + ": not found");
-            }
-        });
-        commandRegistry.register(type);
-        CommandParser commandParser = new CommandParserImp(commandRegistry);
-        System.out.print("$ ");
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            Command command;
-            try {
-                command = commandParser.parse(input);
-            } catch (CommandNotFound e) {
-                Process process = Runtime.getRuntime().exec(new String[]{"which",input.split(" ")[0]});
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line = reader.readLine();
-                if (line != null) {
-                    Process execProcess = Runtime.getRuntime().exec(input);
-                    BufferedReader execReader = new BufferedReader(new InputStreamReader(execProcess.getInputStream()));
-                    String execLine;
-                    while ((execLine = execReader.readLine()) != null) {
-                        System.out.println(execLine);
-                    }
-                } else {
-                    System.out.println(input.split(" ")[0] + ": command not found");
-                }
-                System.out.print("$ ");
-                continue;
-            }
-            command.execute(input.split(" "));
-            System.out.print("$ ");
-        }
+        BShell shell = new BShell();
+        shell.start();
+
 
 
     }
