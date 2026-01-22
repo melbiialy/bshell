014bcb0ec682f7b61879204a697c0d4c5c8f36cc
Elbialy0 <mahmoudelbialy109@gmail.com>
2026-01-22 14:40:39 +0200

    fix AppendRedirectStdout to prepend newline only for non-empty output during append redirection

--- src/main/java/commandexecution/AppendRedirectStdout.java
+++ src/main/java/commandexecution/AppendRedirectStdout.java
@@ -10,8 +10,12 @@ public class AppendRedirectStdout implements Redirect{
     public void redirect(RunResults results, String fileName) {
 
             Path path = BShell.path.getPath().resolve(fileName);
+            String output = results.output();
+            if (!results.output().isEmpty()){
+                output = "\n" + results.output();
+            }
             try {
-                Files.writeString(path,"\n"+results.output(),StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                Files.writeString(path,output,StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             } catch (Exception e) {
                 System.out.println("Error writing to file: " + e.getMessage());
             }
