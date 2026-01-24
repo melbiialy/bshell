50b9fa0a0dfc42acca096686d5baa66e4ab52eb1
Elbialy0 <mahmoudelbialy109@gmail.com>
2026-01-24 22:09:56 +0200

    add support for limiting history output and improve line numbering logic in `HistoryManager`

--- src/main/java/builtincommands/History.java
+++ src/main/java/builtincommands/History.java
@@ -12,10 +12,14 @@ public class History implements BuiltInCommand{
     @Override
     public RunResults operate(String... args) throws IOException, InterruptedException {
         AtomicInteger counter = new AtomicInteger(1);
-        String history = HistoryManager.getHistory()
+        int limit = 0;
+        if (args.length > 0) {
+            limit = Integer.parseInt(args[0]);
+        }
+        String history = HistoryManager.getHistory(limit)
                 .stream()
                 .reduce("",
-                        (a, b) -> (a +"    "+counter.getAndIncrement()+"  "+ b) + "\n");
+                        (a, b) -> (a +"    "+ b) + "\n");
         history = history.trim();
         return new RunResults(history, "");
     }
--- src/main/java/history/HistoryManager.java
+++ src/main/java/history/HistoryManager.java
@@ -4,13 +4,17 @@ import java.util.*;
 
 public class HistoryManager {
     private static Set<String > history;
+    private static int historySize = 0;
     public HistoryManager() {
         history = new LinkedHashSet<>();
     }
     public void add(String command) {
-        history.add(command);
+        history.add((historySize+1) + "  " +command);
+        historySize++;
     }
-    public static Set<String> getHistory() {
-        return history;
+    public static Set<String> getHistory(int limit) {
+        if (historySize < limit) return Collections.emptySet();
+        List<String> historyList = new ArrayList<>(history).subList(limit,historySize);
+        return new LinkedHashSet<>(historyList);
     }
 }
